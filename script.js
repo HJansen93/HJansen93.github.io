c45189de880b3d1c94ae9f7edcf54f0090047713
Henk Jansen <gamerhenkie@hotmail.com>
2015-05-30 12:14:24 +0200

    give this
    a spin

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 
 $(document).ready(function() {
-	
+	Init();
 });
+
+function Init() {
+	var result = generateIndex();
+	createIndex(result);
+}
+function createIndex(items) {
+	for(var i = 0; i < items.count; i++) {
+		$('#sidecontainer').first('#index').append('<div class="indexitem"> <a href="#">' + items[i].text + '</a> </div>');
+	}
+	
+}
+
+function generateIndex() {
+	var index = [];
+	var posts = $('#postcontainer').children('#post');
+	var total = posts.count;
+	posts.each(function(p) {
+		var current = new indexItem( p.children('#postheader').children('#postheadertext').first().text(), null);
+		index[index.length + 1] = current;
+	});
+	
+	return index;
+}
+
+var indexItem = function(title, link) {
+	this.title = title;
+	this.link = link;
+}
